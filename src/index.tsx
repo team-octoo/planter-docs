e14ea5ab967d35f3176bf9fce3d498925920874b
Lennert De Ryck <hello@lennertderyck.be>
2022-09-14 15:17:30 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,7 @@ import App from './App';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Documentation from './sites/Documentation/Documentation';
 import Community from './sites/Community/Community';
-import DocsSectionPage from './sites/Documentation/pages/DocsSectionPage/DocsSectionPage';
-import DocsSectionIndexPage from './sites/Documentation/pages/DocsSectionPage/DocsSectionIndexPage';
-import DocsSubSectionPage from './sites/Documentation/pages/DocsSubSectionPage/DocsSubSectionPage';
-import FlavoursPage from './sites/Documentation/pages/FlavoursPage/FlavoursPage';
 import Main from './sites/Main/Main';
-import ConfigurationExportPage from './sites/Documentation/pages/ConfigurationExportPage/ConfigurationExportPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
